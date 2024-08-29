e0c21ba2d0fc4a549e25f1aee747b421397b0aca
MurilloSSJ <77257498+MurilloSSJ@users.noreply.github.com>
2024-08-28 22:51:26 -0300

    Update model.py

--- src/modules/dag/model.py
+++ src/modules/dag/model.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from src.modules.task.model import BaseTask
 from typing import List, Optional
+from datetime import datetime
 
 
 class DagModel(BaseModel):
@@ -8,5 +9,5 @@ class DagModel(BaseModel):
     description: Optional[str] = None
     schedule: Optional[str] = None
     schedule_interval: Optional[str] = None
-    start_date: Optional[str] = None
+    start_date: Optional[datetime] = None
     tasks: List[BaseTask] = []
